4e503adcf53fac8c01b0f80bb86f8f7e761e6ff9
root <rohitshmahato@gmail.com>
2019-06-25 17:59:54 +0530

    Initial Testing Command Line

---
+++ Test-1/execute_commands.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import subprocess
+
+def open_terminal(app_name, cmd):
+    output = '[+] Executing ' + str(app_name) + '...\r\n'
+    try:
+        output += subprocess.check_output(cmd, shell = True, stderr = subprocess.STDOUT)
+        output += '\r\n'
+    except Exception, e:
+        output += str(e)
+    output += '-------------------------------------------------------\r\n'
+    return output
+
+def execute_commands():
+    # Execute Commands in terminal
+    commands = {"list files":"ls", "current directory":"pwd"}
+    results = ''
+    for key, val in commands.items():
+        output = open_terminal(key, val)
+        results += output
+    return results
+
+def main():
+    print execute_commands()
+
+if __name__ == "__main__":
+    main()
